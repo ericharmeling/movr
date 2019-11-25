1c2a7c86bbad2db306344f794398321497041422
Eric Harmeling <eric.harmeling@cockroachlabs.com>
2019-11-25 14:29:03 -0500

    Changed filter_by to filter; removed revenue

--- movr/transactions.py
+++ movr/transactions.py
@@ -2,22 +2,20 @@ from movr.models import Vehicle, Ride, User
 from sqlalchemy import cast, Numeric
 import datetime
 import uuid
-import decimal
 
 def start_ride_txn(session, city, rider_id, rider_city, vehicle_id):
-    v = session.query(Vehicle).filter_by(city=city, id=vehicle_id).first()
+    v = session.query(Vehicle).filter(Vehicle.city==city, Vehicle.id==vehicle_id).first()
     r = Ride(city=city, id=str(uuid.uuid4()), rider_id=rider_id, rider_city=rider_city, vehicle_id=vehicle_id, start_location=v.last_location)
     session.add(r)
     v.status = "unavailable"
 
 
 def end_ride_txn(session, city, ride_id, location):
-    r = session.query(Ride).filter_by(city=city, id=ride_id).first()
-    v = session.query(Vehicle).filter_by(city=city, id=r.vehicle_id).first()
+    r = session.query(Ride).filter(Ride.city==city, Ride.id==ride_id).first()
+    v = session.query(Vehicle).filter(Vehicle.city==city, Vehicle.id==r.vehicle_id).first()
     r.end_location = location
     r.end_time = datetime.datetime.now()
     r.length = r.end_time - r.start_time
-    r.revenue = 1 + cast(r.length, Numeric)*.02
     v.last_location = location
     v.status = "available"
 
@@ -29,7 +27,7 @@ def add_user_txn(session, city, first_name, last_name, email, username, password
 
 
 def remove_user_txn(session, city, id):
-    u = session.query(User).filter_by(city=city, id=id).first()
+    u = session.query(User).filter(User.city==city, User.id==id).first()
     u.username = None
 
 
@@ -38,17 +36,17 @@ def add_vehicle_txn(session, city, owner_id, last_location, type, color, brand, 
     v = Vehicle(id=str(uuid.uuid4()), type=vehicle_type, city=city, owner_id=owner_id, last_location = last_location, color=color, brand=brand, status=status)
     session.add(v)
     if not is_owner:
-        u = session.query(User).filter_by(id=v.owner_id).first()
+        u = session.query(User).filter(User.id==v.owner_id).first()
         u.is_owner = True
 
 
 def remove_vehicle_txn(session, city, id):
-    v = session.query(Vehicle).filter_by(city=city, id=id).first()
+    v = session.query(Vehicle).filter(Vehicle.city==city, Vehicle.id==id).first()
     v.status = 'removed'
     owner = v.owner_id
     vehicles = session.query(Vehicle).filter(Vehicle.city==city, Vehicle.owner_id==owner, Vehicle.status!='removed').all()
     if not vehicles:
-        u = session.query(User).filter_by(city=city, id=owner).first()
+        u = session.query(User).filter(User.city==city, User.id==owner).first()
         u.is_owner = False
 
 
@@ -59,9 +57,9 @@ def get_users_txn(session, city):
 
 def get_user_txn(session, username=None, user_id=None):
     if username:
-        u = session.query(User).filter_by(username=username).first()
+        u = session.query(User).filter(User.username==username).first()
     elif user_id:
-        u = session.query(User).filter_by(id=user_id).first()
+        u = session.query(User).filter(User.id==user_id).first()
     if u:
         session.expunge(u)
     return u
@@ -73,6 +71,6 @@ def get_vehicles_txn(session, city):
 
 
 def get_rides_txn(session, rider_id):
-    rides = session.query(Ride).filter_by(rider_id=rider_id).order_by(Ride.start_time).all()
-    return list(map(lambda ride: {'city': ride.city, 'id': ride.id, 'vehicle_id': ride.vehicle_id, 'start_time': ride.start_time, 'end_time': ride.end_time, 'rider_id': ride.rider_id, 'length': ride.length, 'revenue': ride.revenue}, rides))
+    rides = session.query(Ride).filter(Ride.rider_id==rider_id).order_by(Ride.start_time).all()
+    return list(map(lambda ride: {'city': ride.city, 'id': ride.id, 'vehicle_id': ride.vehicle_id, 'start_time': ride.start_time, 'end_time': ride.end_time, 'rider_id': ride.rider_id, 'length': ride.length}, rides))
 
